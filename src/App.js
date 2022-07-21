a1e50cb4a3f178ed20c7505fe2a64f2a93378e53
sakina175 <sakina.1701175@gmail.com>
2022-07-21 09:25:50 +0500

    setup changes -I

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Navbar } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+    <h1>sakina</h1>
+    </div>
+  );
+}
+
+export default App;
